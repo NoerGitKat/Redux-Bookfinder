74d36e175d8bdc16d5e4ce74ace316205748e4b0
Noer Paanakker <noer@healthyworkers.nl>
2017-09-29 10:35:29 +0200

    created starter files

---
+++ src/containers/AppContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <div>React simple starter</div>
+    );
+  }
+}
+
+
+export default AppContainer;
